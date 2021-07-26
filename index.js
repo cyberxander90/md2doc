8a830074fd0067a0b157a4eb1b527b09092146f1
Alexander <aleyva@recurly.com>
2021-07-25 21:39:52 -0300

    Create a cli.

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
+#!/usr/bin/env node
+
+const yargs = require('yargs');
 const md2docs = require('./lib/md2docs');
 
-md2docs({ output: 'testing/site', target: 'testing/doc' });
+// eslint-disable-next-line no-unused-expressions
+yargs
+  .scriptName('md2docs')
+  .usage('$0 [args]')
+  .command(
+    '$0',
+    'the default command',
+    () => {},
+    (argv) => {
+      md2docs({
+        output: argv.outputDir || 'testing/site',
+        target: argv.inputDir || 'testing/doc',
+      });
+    },
+  )
+  .option('inputDir', {
+    alias: 'i',
+    description: `Path to directory to extract the documentation.
+    Default is './doc'.`,
+  })
+  .option('outputDir', {
+    alias: 'o',
+    description: `Path to directory to create the web site documentation.
+    Default is './site'.`,
+  })
+  .option('version', {
+    alias: 'v',
+    description: 'Show version number.',
+  })
+  .option('help', {
+    alias: 'h',
+    description: 'Show help.',
+  })
+  .argv;
